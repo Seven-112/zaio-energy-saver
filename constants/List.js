4e39fe7f43e49fb31d024bbbf600319fdd84595b
myshine112 <bluesatellite777@gmail.com>
2021-11-03 14:38:02 +0200

    Add devices list

---
+++ constants/List.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: 1,
+        name : 'Light',
+        location: 'Living Room',
+        active: true,
+        state : 58,
+        icon : 'light'
+    },
+    {
+        id: 2,
+        name : 'Air conditionner',
+        location: 'Bed Room',
+        active: false,
+        state : 25,
+        icon: 'ac'
+    },
+    {
+        id: 3,
+        name : 'CCTV',
+        location: 'Outdoor',
+        active: false,
+        state : 73,
+        icon: 'camera'
+    }
+];
--- constants/Size.js
+++ constants/Size.js
@@ -1,3 +1,11 @@
+import { Dimensions } from "react-native";
+
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
+
 export default {
-    m_5: 5
+    m_5: 5,
+    height,
+    width,
+    p: 5
 };
