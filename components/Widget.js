d8f922a8beb0667fca1052f97048b359c4b2f936
myshine112 <bluesatellite777@gmail.com>
2021-11-03 14:51:54 +0200

    Add widget component

---
+++ components/Widget.js
@@ -0,0 +1,57 @@
+import { Block, Text, theme } from "galio-framework";
+import React from "react";
+import { Image, Switch, TouchableOpacity, View } from "react-native";
+import PropTypes from 'prop-types';
+import Size from "../constants/Size";
+
+
+const Widget = (props) => {
+
+    const progress = () => {
+        return (
+          <View style={{marginTop:15, width: '100%', justifyContent:'center', borderRadius: 10, 
+          backgroundColor: props.widget.active? '#64d1da': '#ddd', height:28}}> 
+            <View style={{ width: props.widget.state+'%', height: 28, 
+            backgroundColor: props.widget.state < 20 ?
+            theme.COLORS.DRIBBBLE : props.widget.state < 50 ? theme.COLORS.WARNING: 
+            props.widget.state < 90? 
+            props.widget.active? theme.COLORS.WHITE: theme.COLORS.TWITTER : theme.COLORS.SUCCESS,  borderRadius: 10,  }}>
+            
+            </View>
+            <View style={{position:'absolute', width:'100%',alignItems:'flex-end', paddingRight: 15}}>
+              <Text color={props.widget.active? 'white': '#000'} >{props.widget.state}%</Text>
+            </View>
+          </View>
+        )
+      }
+
+    return (
+        <Block style={{ width: Size.width / 2.11, padding: Size.p}}>
+
+        <Block>
+          <TouchableOpacity style={{ height: 160, 
+          backgroundColor: props.widget.active? theme.COLORS.TWITTER: theme.COLORS.WHITE, 
+          borderRadius: 10, padding: Size.p, elevation:2}}>
+            <Block row space="between" style={{ alignItems:'center'}}>
+              <Block middle shadow style={{ backgroundColor: props.widget.active? theme.COLORS.WHITE: theme.COLORS.GREY, borderRadius: 35, width: 35, height: 35, alignItems:'center'}}>
+                <Image source={require('../assets/favicon.png')} style={{width: 20, height: 20}} />
+              </Block>
+              <Switch value={props.widget.active} trackColor={{
+                false: theme.COLORS.GREY,
+                true: theme.COLORS.WHITE
+              }} />
+            </Block>
+            <Text bold color={props.widget.active? 'white': 'black'} style={{marginTop: Size.m_5*2.9}}>{props.widget.name}</Text>
+            <Text color={props.widget.active? 'white': 'gray'} small>{props.widget.location}</Text>
+            { progress()}
+          </TouchableOpacity>
+        </Block>
+      </Block>
+    )
+};
+
+Widget.propTypes = {
+    widget: PropTypes.object,
+  }
+
+export default Widget;
--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,34 @@
-import { Block, Text, Button, theme } from "galio-framework";
-import React from "react";
-import { StyleSheet, Image } from "react-native";
+import { Block, Text, Button, theme, Card, Icon } from "galio-framework";
+import React, { useState } from "react";
+import { StyleSheet, Image, TouchableOpacity, ScrollView } from "react-native";
+import Widget from "../components/Widget";
+import List from "../constants/List";
 import Size from "../constants/Size";
 
 
 const Home = () => {
+
+    const [list, setList] = useState([...List]);
+
+    const addList = () => {
+        list.push({
+            id: 5,
+            name : 'TV 2',
+            location: 'Living Room',
+            active: true,
+            state : 49,
+            icon: 'ac'
+        });
+
+        setList([...list]);
+    };
+
+
     return(
+        <ScrollView>
         <Block style={styles.container} flex={1}>
             <Block space="between" row middle>
-                <Block row style={styles.m}>
+                <Block row >
                     <Block style={styles.m}>
                         <Image source={require("../assets/profile.jpg")} style={styles.profile} />
                     </Block>
@@ -28,7 +48,59 @@ const Home = () => {
                 </Button>
 
             </Block>
+
+            <Block style={styles.card} >
+                <Block style={styles.energy} row space="between">
+                    <Text bold size={15}>Energy consuming</Text>
+                    <Block row middle>
+                        <Text>24 sep 2021</Text>
+                        <Icon name="tags" family="antdesign" color={theme.COLORS.BLACK} size={10} />
+                    </Block>
+                </Block>
+                <Block style={styles.energyState} row space="between">
+                    <Block row>
+                        <Icon name="tags" family="antdesign" color={theme.COLORS.TWITTER} size={50} />
+                        <Block>
+                            <Text muted>Today</Text>
+                            <Text bold>24 kwH</Text>
+                        </Block>
+                    </Block>
+                    <Block row>
+                        <Icon name="tags" family="antdesign" color={theme.COLORS.TWITTER} size={50} />
+                        <Block>
+                            <Text muted>This Month</Text>
+                            <Text bold>424 kwH</Text>
+                        </Block>
+                    </Block>
+                </Block>
+            </Block>
+            <Text bold size={20}>Connected devices</Text>
+            <Block style={styles.wrap} row>
+                {
+                    list.map((obj, index)=> 
+                        <Widget widget={obj}/>
+                    )
+                }
+                <Block style={styles.addWidget}>
+                    <TouchableOpacity style={styles.add}
+                        onPress={()=> addList()}
+                    >
+                        <Button 
+                            onlyIcon 
+                            icon="tags" 
+                            iconFamily="antdesign" 
+                            iconSize={30} 
+                            color="white" 
+                            iconColor="grey" 
+                            style={{ width: 55, height: 55, elevation: 5 }}>
+                        </Button>
+                        <Text bold>Add device</Text>
+                    </TouchableOpacity>
+                </Block>
+            </Block>
+
         </Block>
+        </ScrollView>
     )
 };
 
@@ -39,10 +111,42 @@ const styles = StyleSheet.create({
         borderRadius: 25
     },
     container:{
-        backgroundColor: theme.COLORS.GREY
+        backgroundColor: theme.COLORS.GREY,
+        padding:12
     },
     m: {
         margin: Size.m_5
+    },
+    energy: {
+        margin: Size.m_5,
+        padding: 15,
+        borderBottomColor: theme.COLORS.MUTED,
+        borderBottomWidth:1,
+    },
+    energyState: {
+        margin: Size.m_5,
+        paddingHorizontal: 15
+    },
+    card: {
+        marginVertical:20,
+        backgroundColor: theme.COLORS.WHITE,
+        borderRadius: 10,
+        elevation: 0.2
+    },
+    wrap :{
+        flexWrap: "wrap"
+    },
+    addWidget: {
+        width: Size.width/2.15,
+        padding: Size.p
+    },
+    add: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 160,
+        borderWidth: 2,
+        borderStyle: "dashed",
+        borderRadius: 10
     }
 });
 
